345817a9e4ca0a2e4395d03c5cb1992307fcce0f
dmn28 <damjantzar@gmail.com>
2021-04-24 00:41:44 +0200

    [feat] Add diffuse and specular ligthing and a rotating cube
    The added cube is a light source, and it rotates above the pyramid. *Also changed the pyramid making from with EBO to without EBO because of the normals for the light.

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,31 @@
 #include <learnopengl/filesystem.h>
 #include <learnopengl/shader_m.h>
 #include <learnopengl/camera.h>
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
-
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-
-float deltaTime = 0.0f;	// time between current frame and last frame
-float lastFrame = 0.0f;
+void processInput(GLFWwindow *window);
+unsigned int loadTexture(const char *path);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// lighting
+glm::vec3 lightPos(1.2f, 3.0f, 2.0f);
 
 int main()
 {
@@ -44,7 +50,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Piramide", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -56,6 +62,7 @@ int main()
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
+    // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: load all OpenGL function pointers
@@ -70,87 +77,131 @@ int main()
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
-    // build and compile our shader zprogram
+    // shaders
     // ------------------------------------
-    Shader ourShader("resources/shaders/pyramid.vs","resources/shaders/pyramid.fs");
+    Shader pyramidShader("resources/shaders/pyramid.vs", "resources/shaders/pyramid.fs");
+    Shader lightCubeShader("resources/shaders/light_cube.vs", "resources/shaders/light_cube.fs");
 
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
     float vertices[] = {
-            // coords            colors        texture
-            0.5f,  -0.5f, 0.5f,   1,1,0,    0,0,
-            0.5f, -0.5f, -0.5f,   1,1,0,    1,0,
-            -0.5f, -0.5f, -0.5f,  1,1,0,    0,0,
-            -0.5f,  -0.5f, 0.5f,  1,1,0,    1,0,
-            0.0f, 0.5f, 0.0f,     0,0,0,    0.5,1
+            // positions      normals    texture coords
+            -0.5,-0.5,-0.5,  0,0.5,-1,      0,0,
+            0.5,-0.5,-0.5,   0,0.5,-1,      1,0,
+            0,0.5,0,         0,0.5,-1,      0.5,1,
+
+            0.5,-0.5,-0.5,   1,0.5,0,       1,0,
+            0.5,-0.5,0.5,    1,0.5,0,       0,0,
+            0,0.5,0,         1,0.5,0,       0.5,1,
+
+            0.5,-0.5,0.5,    0,0.5,1,       0,0,
+            -0.5,-0.5,0.5,   0,0.5,1,       1,0,
+            0,0.5,0,         0,0.5,1,       0.5,1,
+
+            -0.5,-0.5,0.5,   -1,0.5,0,      1,0,
+            -0.5,-0.5,-0.5,  -1,0.5,0,      0,0,
+            0,0.5,0,         -1,0.5,0,      0.5,1,
+
+            -0.5,-0.5,-0.5,   0,1,0,        0,0,
+            0.5,-0.5,-0.5,    0,1,0,        1,0,
+            0.5,-0.5,0.5,     0,1,0,        0,0,
+
+            -0.5,-0.5,-0.5,   0,1,0,        0,0,
+            0.5,-0.5,0.5,    0,1,0,         0,0,
+            -0.5,-0.5,0.5,   0,1,0,         1,0,
     };
 
-    unsigned int indices[] = {
-            0,1,3,
-            1,2,3,
+    //configure the pyramid's VAO and VBO
+    unsigned int pyramidVBO, pyramidVAO;
+    glGenVertexArrays(1, &pyramidVAO);
+    glGenBuffers(1, &pyramidVBO);
 
-            1,2,4,
-            0,1,4,
-            0,3,4,
-            2,3,4
-    };
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, pyramidVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
+    glBindVertexArray(pyramidVAO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
-    glVertexAttribPointer(2,2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
+
+    // configure the light's VAO,VBO and EBO
+    float lightCube_vertices[] = {
+            // front
+            -0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            // back
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            -0.5f,  0.5f, -0.5f
+    };
+
+    int lightCube_indices[] = {
+            // front
+            0, 1, 2,
+            2, 3, 0,
+            // right
+            1, 5, 6,
+            6, 2, 1,
+            // back
+            7, 6, 5,
+            5, 4, 7,
+            // left
+            4, 0, 3,
+            3, 7, 4,
+            // bottom
+            4, 5, 1,
+            1, 0, 4,
+            // top
+            3, 2, 6,
+            6, 7, 3
+    };
+
+    unsigned int lightCubeVAO, lightCubeVBO, lightCubeEBO;
+    glGenVertexArrays(1, &lightCubeVAO);
+    glGenBuffers(1, &lightCubeVBO);
+    glGenBuffers(1, &lightCubeEBO);
+    glBindVertexArray(lightCubeVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, lightCubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(lightCube_vertices), lightCube_vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, lightCubeEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(lightCube_indices), lightCube_indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     glBindVertexArray(0);
 
 
-    // load and crate texture
-    unsigned int texture_pyramid;
 
-    glGenTextures(1,&texture_pyramid);
-    glBindTexture(GL_TEXTURE_2D, texture_pyramid);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-    //filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load textures (we now use a utility function to keep the code more organized)
+    // -----------------------------------------------------------------------------
+    unsigned int diffuseMap = loadTexture(FileSystem::getPath("resources/textures/brickwall.jpg").c_str());
+    unsigned int specularMap = loadTexture(FileSystem::getPath("resources/textures/brickwall.jpg").c_str());
 
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data = stbi_load(FileSystem::getPath("resources/textures/brickwall.jpg").c_str(), &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
+    // shader configuration
+    // --------------------
+    pyramidShader.use();
+    pyramidShader.setInt("material.diffuse", 0);
+    pyramidShader.setInt("material.specular", 1);
 
-    ourShader.use();
 
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-
+        // per-frame time logic
+        // --------------------
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
@@ -164,35 +215,60 @@ int main()
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        //activate shader when setting uniforms/drawing objects
+        pyramidShader.use();
+        pyramidShader.setVec3("light.position", lightPos);
+        pyramidShader.setVec3("viewPos", camera.Position);
 
-        // activate shader
+        //lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
+        //lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
 
-        glBindTexture(GL_TEXTURE_2D, texture_pyramid);
+        // ligthpos in circle (room for tweeking)
+        lightPos.x = 3.0f * cos(glfwGetTime());
+        lightPos.z = 3.0f * sin(glfwGetTime());
 
-        ourShader.use();
+        // light properties
+        pyramidShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+        pyramidShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+        pyramidShader.setVec3("light.specular", 1.0f, 0.85f, 0.0f);
 
-        // pass projection matrix to shader (note that in this case it could change every frame)
+        // material properties
+        pyramidShader.setFloat("material.shininess", 64.0f);
+
+        // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        ourShader.setMat4("projection", projection);
-
-        // camera/view transformation
         glm::mat4 view = camera.GetViewMatrix();
-        ourShader.setMat4("view", view);
+        pyramidShader.setMat4("projection", projection);
+        pyramidShader.setMat4("view", view);
 
-        glm::mat4 model = glm::mat4(1.0f); 
-        model = glm::translate(model, glm::vec3(0.0f,0.0f,0.0f));
-        //model = glm::rotate(model, -45.0f, glm::vec3(2.0f, -0.5f, 1.0f));
-        model = glm::scale(model,glm::vec3(1.0f,1.0f,1.0f));
-        ourShader.setMat4("model", model);
+        // world transformation
+        glm::mat4 model = glm::mat4(1.0f);
+        pyramidShader.setMat4("model", model);
+
+        // bind diffuse map
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseMap);
+        // bind specular map
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specularMap);
+
+        // render the pyramid
+        glBindVertexArray(pyramidVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
+        // draw the lightCube object
+        lightCubeShader.use();
+        lightCubeShader.setMat4("projection", projection);
+        lightCubeShader.setMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+        lightCubeShader.setMat4("model", model);
 
-
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0); // no need to unbind it every time
-
+        glBindVertexArray(lightCubeVAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 36);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -202,7 +278,11 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-
+    glDeleteVertexArrays(1, &pyramidVAO);
+    glDeleteVertexArrays(1, &lightCubeVAO);
+    glDeleteBuffers(1, &pyramidVBO);
+    glDeleteBuffers(1, &lightCubeVBO);
+    glDeleteBuffers(1, &lightCubeEBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -236,6 +316,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
     if (firstMouse)
@@ -259,4 +341,43 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
+}
+
+// utility function for loading a 2D texture from file
+// ---------------------------------------------------
+unsigned int loadTexture(char const * path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
 }
